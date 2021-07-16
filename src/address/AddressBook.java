b8bf16c05bef7e0f2079b2e88849aa2d7480686f
lavanya-pg <lavanyapg8@gmail.com>
2021-07-16 07:53:02 +0530

    ADDED: Code For Counting The Number OF Contacts

--- src/address/AddressBook.java
+++ src/address/AddressBook.java
@@ -1,11 +1,12 @@
 package address;
 
-import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Scanner;
 
-public class AddressBook
-{
+public class AddressBook {
+
 	HashMap<String, ArrayList<Contact>> addressBooks = new HashMap<>();
 	ArrayList<Contact> allContacts = new ArrayList<Contact>();
 	Scanner scanner = new Scanner(System.in);
@@ -34,22 +35,25 @@ public class AddressBook
 		{
 			ArrayList<Contact> contactList = addressBooks.get(bookName);
 			addContactToExsistingBook(contact, bookName, contactList);
-		} 
-		else
-		{
+		} else {
+			// creating a new book and list
 			allContacts.add(contact);
 			addressBooks.put(bookName, allContacts);
-			System.out.println("created a address book and Contact Added Sucessfully");
+			System.out.println("New book created and Contact Added Sucessfully");
 		}
 
 		return contact;
 	}
-	public boolean editContact(String phoneNumber)
-	{
-		for (Contact contact : allContacts) 
-		{
-			if (contact.getPhoneNumber() == phoneNumber)
-			{
+
+	/**
+	 * Method to Edit contact using unique phoneNumber
+	 * 
+	 * @param phoneNumber
+	 * @return
+	 */
+	public boolean editContact(String phoneNumber) {
+		for (Contact contact : allContacts) {
+			if (contact.getPhoneNumber() == phoneNumber) {
 				System.out.println("Enter First Name");
 				String firstName = scanner.next();
 				System.out.println("Enter last Name");
@@ -70,44 +74,51 @@ public class AddressBook
 		}
 		return operationStatus(false);
 	}
-	public boolean deleteContact(String phoneNumber) 
-	{
 
-		for (Contact contact : allContacts) 
-		{
-			if (contact.getPhoneNumber() == phoneNumber)
-			{
+	/**
+	 * Method to Delete contact using unique phoneNumber
+	 * 
+	 * @param phoneNumber
+	 * @return
+	 */
+	public boolean deleteContact(String phoneNumber) {
+
+		for (Contact contact : allContacts) {
+			if (contact.getPhoneNumber() == phoneNumber) {
 				allContacts.remove(contact);
 				return operationStatus(true);
 			}
 		}
 		return operationStatus(false);
 	}
-	public void displayContacts(ArrayList<Contact> contactList)
-	{
-		for (Contact contact : contactList)
-		{
+
+	/**
+	 * Method to Display the Contact Details
+	 */
+	public void displayContacts(LinkedList<Contact> contactList) {
+		for (Contact contact : contactList) {
 			System.out.println(contact);
 		}
 	}
 
-	public void displayContact() 
-	{
-		for (String bookName : addressBooks.keySet()) 
-		{
+	public void displayContact() {
+		for (String bookName : addressBooks.keySet()) {
 			System.out.println(bookName);
-			ArrayList<Contact> contactList = addressBooks.get(bookName);
+			LinkedList<Contact> contactList = addressBooks.get(bookName);
 			displayContacts(contactList);
 		}
 	}
-	private static boolean operationStatus(boolean status) 
-	{
-		if (status)
-		{
+
+	/**
+	 * Method to check the status of operation whether it is done properly or not
+	 * 
+	 * @param status
+	 * @return
+	 */
+	private static boolean operationStatus(boolean status) {
+		if (status) {
 			System.out.println("Contact Updated Successfully");
-		} 
-		else 
-		{
+		} else {
 			System.out.println("Contact not found");
 		}
 		return status;
@@ -115,30 +126,31 @@ public class AddressBook
 
 	private void addContactToExsistingBook(Contact contact, String bookName, ArrayList<Contact> contactList) {
 		boolean isAlreadyExsist = false;
-		for (Contact searchContact : contactList)
+		for (Contact searchContact : contactList) 
 		{
-			if (searchContact.getFirstName().equals(contact.getFirstName()))
+			if (searchContact.getFirstName().equals(contact.getFirstName())) 
 			{
 				isAlreadyExsist = true;
 				break;
 			}
 		}
-		if (!(isAlreadyExsist)) {
+		if (!(isAlreadyExsist))
+		{
 			contactList.add(contact);
 			addressBooks.put(bookName, contactList);
 			System.out.println("New Contact Added Sucessfully");
-		} else {
+		} 
+		else 
+		{
 			System.out.println("Contact already exsist");
 		}
 	}
-
 	public int searchPerson(String searchKey)
 	{
 		int count = 0;
-		for (String bookName : addressBooks.keySet())
-		{
+		for (String bookName : addressBooks.keySet()) {
 			ArrayList<Contact> contactList = addressBooks.get(bookName);
-			for (Contact contact : contactList) 
+			for (Contact contact : contactList)
 			{
 				if (contact.getCity().equals(searchKey) || contact.getState().equals(searchKey)) 
 				{
@@ -147,17 +159,16 @@ public class AddressBook
 				}
 			}
 		}
-		return count;
+		return count; 
 	}
-
 	public void viewPerson(String viewKey)
 	{
-		for (String bookName : addressBooks.keySet())
+		for (String bookName : addressBooks.keySet()) 
 		{
 			ArrayList<Contact> contactList = addressBooks.get(bookName);
 			for (Contact contact : contactList) 
 			{
-				if (contact.getCity().equals(viewKey) || contact.getState().equals(viewKey))
+				if (contact.getCity().equals(viewKey) || contact.getState().equals(viewKey)) 
 				{
 					System.out.println(contact);
 				}
