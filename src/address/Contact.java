3cd896fef1b1a2f5bb48be239f3ebb2ed18b0ebe
lavanya-pg <lavanyapg8@gmail.com>
2021-07-25 13:21:48 +0530

    ADDED: Ability to Sort the Entries by Name in AddressBook

--- src/address/Contact.java
+++ src/address/Contact.java
@@ -3,82 +3,109 @@ package address;
 public class Contact
 {
 
-	public String firstName;
-	public String lastName;
-	public String address;
-	public String city;
-	public String state;
-	public int zip;
-	public long phoneNo;
-	public String email;
+	String firstname;
+	String lastname;
+	String city;
+	String state;
+	int zipcode;
+	String phonenumber;
+	String email;
 	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
+	public Contact(String firstname, String lastname, String city, String state, int zipcode,
+			String phonenumber,String email) 
+	{
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
 		this.city = city;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
 		this.state = state;
-	}
-	public int getZip() {
-		return zip;
-	}
-	public void setZip(int zip) {
-		this.zip = zip;
-	}
-	
-	public long getPhoneNo() {
-		return phoneNo;
-	}
-	public void setPhoneNo(long phoneNo) {
-		this.phoneNo = phoneNo;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
+		this.zipcode = zipcode;
+		this.phonenumber = phonenumber;
 		this.email = email;
 	}
-	
-	
-	public Contact(String f,String l,String a,String c,String s,int z,long p,String e) {
-		// TODO Auto-generated constructor stub
-		setFirstName(f);
-		setLastName(l);
-		setAddress(a);
-		setCity(c);
-		setState(s);
-		setZip(z);
-		setPhoneNo(p);
-		setEmail(e);
+
+
+	public Contact() 
+	{
+	}
+
+
+	public String getFirstname() 
+	{
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) 
+	{
+		this.firstname = firstname;
+	}
+
+	public String getLastname() 
+	{
+		return lastname;
+	}
+
+	public void setLastname(String lastname) 
+	{
+		this.lastname = lastname;
+	}
+
+	public String getCity() 
+	{
+		return city;
+	}
+
+	public void setCity(String city) 
+	{
+		this.city = city;
+	}
+
+	public String getState() 
+	{
+		return state;
+	}
+
+	public void setState(String state) 
+	{
+		this.state = state;
+	}
+
+	public int getZipcode() 
+	{
+		return zipcode;
+	}
+
+	public void setZipcode(int zipcode) 
+	{
+		this.zipcode = zipcode;
+	}
+
+	public String getPhonenumber() 
+	{
+		return phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber)
+	{
+		this.phonenumber = phonenumber;
 	}
 	
+	public String getEmail() 
+	{
+		return email;
+		
+	}
 	
+	public void setEmail(String email) 
+	{
+		this.email = email;
+	}
+
 	@Override
-	public String toString() {
-		return "Contact Details: "+firstName +" "+ lastName+ ", "+address+", "+city+", " +state+ ", "+zip+", "+ phoneNo + ", "+email+" ";
+	public String toString() 
+	{
+		return "Contact [firstname=" + firstname + ", lastname=" + lastname + ", city=" + city + ", state="
+				+ state + ", zipcode=" + zipcode + ", phonenumber=" + phonenumber + ",email="+email+"]";
 	}
+
 }
